a6193405a98620dcd51ba285279413dfc3837fff
Gavin Aldred <Gavin.Aldred@nationaltimbergroup.com>
2023-01-27 23:22:43 +0000

    local storage work to persist buttons, also saving json object to an array - not sure what we're gonna do with it yet!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,65 @@
-
 //Set up the API key
-
-
 var APIKey = "8a0b8a21179a203dc24ac4d2fef35efa";
-var city = "london"
+var city = "london";
+var searchResults = [];
 
 // Here we are building the URL we need to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + APIKey;
 
 $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-  
-console.log(response)
-    // Create CODE HERE to calculate the temperature (converted from Kelvin)
-  
-// Get the submit button
-var submitButton = document.getElementById("search-button");
-
-submitButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    // Get the city name from the input field
-    city = document.getElementById("search-input").value;
-    // Create a new button element
-    var newButton = document.createElement("button");
-    // Set the text of the button to be the city name
-    newButton.innerHTML = city;
-    // Add a class to the button for styling
-    newButton.classList.add("list-group-item-button");
-    newButton.classList.add("list-group-item");
-    // Append the button to the history div
-    document.getElementById("history").appendChild(newButton);
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  // Get the submit button
+  var submitButton = document.getElementById("search-button");
+  submitButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      // Get the city name from the input field
+      city = document.getElementById("search-input").value;
+      // Here we are building the URL we need to query the database
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&appid=" + APIKey;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          // Create a new button element
+          var newButton = document.createElement("button");
+          // Set the text of the button to be the city name
+          newButton.innerHTML = response.name;
+          // Add a class to the button for styling
+          newButton.classList.add("list-group-item-button");
+          newButton.classList.add("list-group-item");
+          // Append the button to the history div
+          document.getElementById("history").appendChild(newButton);
+          //save search results to array
+          searchResults.push(response);
+          //save searchResults array to localStorage
+          localStorage.setItem("searchResults", JSON.stringify(searchResults));
+      });
   });
+
+  // Clear Searches button
+var clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", function(event) {
+event.preventDefault();
+localStorage.removeItem("searchResults");
+searchResults = [];
+// Select all list items but not the clear results button
+$("#history li:not(#clear-button)").remove();
+location.reload();
+});
   
 
-  }); 
-  
-
-      
-// Here we display the 5 days forecast for the current city.
-function forecast(cityid){
-cityid = city;
-  var queryForcastURL="https://api.openweathermap.org/data/2.5/forecast?q="+cityid+"&appid="+APIKey;
-  $.ajax({
-      url:queryForcastURL,
-      method:"GET"
-  })
-  console.log(cityid)
-}
-
-forecast()  
+  //retrieve searchResults from localStorage
+  if(localStorage.getItem("searchResults")){
+      searchResults = JSON.parse(localStorage.getItem("searchResults"));
+      // Iterate over the searchResults array to create buttons for each city
+      for (var i = 0; i < searchResults.length; i++) {
+          var newButton = document.createElement("button");
+          newButton.innerHTML = searchResults[i].name;
+          newButton.classList.add("list-group-item-button");
+          newButton.classList.add("list-group-item");
+          document.getElementById("history").appendChild(newButton);
+      }
+  }
+});
